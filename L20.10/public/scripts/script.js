a1c9cfe90faec533317feb542234a4f8b240270e
SerpNaga <TyurinaAA20@st.ithub.ru>
2021-10-29 13:21:02 +0300

    10th

--- L20.10/public/scripts/script.js
+++ L20.10/public/scripts/script.js
@@ -7,5 +7,29 @@ let data = [
 
 const outr = document.querySelector(".container")
 data.forEach(el => {
-    outr.innerHTML += `<div class="card">${el.name}</div>`
+    outr.innerHTML += `<div data-name="${el.name}" data-src="${el.src}" class="card">${el.name}</div>`
+})
+
+let other = document.createElement('div')
+other.className = "card"
+other.setAttribute("data-src", "../images/ex.png")
+let inp = document.createElement('input')
+inp.setAttribute("placeholder", "Enter your name")
+inp.addEventListener('click', e=>{
+    e.stopPropagation()
+})
+inp.addEventListener('input', e=>{
+    e.target.parentElement.setAttribute("data-name", e.target.value)
+})
+other.append(inp)
+outr.append(other)
+
+cards = document.querySelectorAll(".card").forEach(el => {
+    el.addEventListener('click', e => {
+        //location.reload() - перезагрузить
+    /*
+
+    */
+        location.replace(`./personal?name=${e.target.getAttribute('data-name')}&img=${e.target.getAttribute('data-src')}`)
+    })
 })
--- L20.10/server/routes/server.js
+++ L20.10/server/routes/server.js
@@ -1,4 +1,20 @@
 const router = require("express").Router()
 
+router.get('/personal', (rq, rs)=>{
+    const name = rq.query.name
+    const img = rq.query.img
+    //console.log(rq.query)
+    rs.send(
+        `<div>
+            <img style="width: 300px;
+            height: 300px;
+            border-radius: 50%;
+            object-position: center;
+            object-fit: cover;" src="${img}" alt="${name}">
+            <h1>${name}</h1>
+            <a href="../">back</a>
+        </div>`
+    )
+})
 
 module.exports = router
